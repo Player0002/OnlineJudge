e5d6fdd9473307c981236be2ae09279e06cbe445
김영재 <dudwo0914@gmail.com>
2020-04-12 15:33:43 +0900

    Updates

---
+++ precache-manifest.8701f467071057a93d5802e2dc90b493.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc290ba218c561a0624bb7ec7f7292e",
+    "url": "/OnlineJudge/index.html"
+  },
+  {
+    "revision": "b84e1f506c798b93b5ec",
+    "url": "/OnlineJudge/static/css/main.9a0549bc.chunk.css"
+  },
+  {
+    "revision": "712fcac98368c1f744db",
+    "url": "/OnlineJudge/static/js/2.ab9f4c99.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "/OnlineJudge/static/js/2.ab9f4c99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b84e1f506c798b93b5ec",
+    "url": "/OnlineJudge/static/js/main.d6116c90.chunk.js"
+  },
+  {
+    "revision": "aab277240ec1c4a746d7",
+    "url": "/OnlineJudge/static/js/runtime-main.c8310d1e.js"
+  },
+  {
+    "revision": "97d4289a00e513cc6c6a290857c8f54a",
+    "url": "/OnlineJudge/static/media/danny_logo.97d4289a.png"
+  }
+]);
